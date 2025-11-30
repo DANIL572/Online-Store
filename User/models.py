fb65ab3e700f19dd3ad0b1cabd0d6397f8e245ec
Danil <danilsavrin93@gmail.com>
2025-11-30 15:26:04 +0300

    Добавил модель пользователя

---
+++ User/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+# Create your models here.
+from encodings.punycode import digits
+from django.contrib.auth.models import AbstractUser
+
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxLengthValidator
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    first_name = None
+    last_name = None
+
+    # ОБЯЗАТЕЛЬНО добавь эти поля с related_name!
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='customuser_groups',  # ← УНИКАЛЬНЫЙ related_name
+        related_query_name='user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='customuser_permissions',  # ← УНИКАЛЬНЫЙ related_name
+        related_query_name='user',
+    )
+
+    phone = models.CharField(max_length=12, unique=True, verbose_name='phone', blank=True)
+    balance = models.DecimalField(max_digits=15, decimal_places=2, validators=[MinValueValidator(0)], default=0.00)
+    is_seller = models.BooleanField(default=False, verbose_name='seller')
+
+
+    def check_phone(self):
+        if len(self.phone) != 12:
+             raise ValidationError('Номер должен состоять из 12 символов')
+        if not self.phone.startswith('+'):
+            raise ValidationError('Номер должен начинаться с +')
+        if not self.phone[1:].isdigit():
+            raise ValidationError('Номер должен состоять из цифр после знака +')
+
+
+
+    def save(self, *args, **kwargs):
+        if not self.is_superuser and self.phone:
+            self.check_phone()
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return f'Имя : {self.name.title()}, Фамилия : {self.surname.title()}'
+
