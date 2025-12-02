bc454815887d0fbb46cd6cd1379d0576194722b2
Danil <danilsavrin93@gmail.com>
2025-12-02 19:16:34 +0300

    Добавлено представление регистрации

---
+++ Auth/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login, authenticate
+from User.models import CustomUser
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('user_detail', id=user.id)
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
