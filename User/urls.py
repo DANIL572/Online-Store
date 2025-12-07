69e673f607abc04214e13998ba2a2ae7cb9b7fc5
Danil <danilsavrin93@gmail.com>
2025-12-07 16:15:17 +0300

    Убрал пути к пополнению и снятию

--- User/urls.py
+++ User/urls.py
@@ -3,6 +3,4 @@ from .views import CustomUserView, deposit, withdraw
 
 urlpatterns = [
     path('<int:id>/', CustomUserView.as_view(), name='user_detail'),
-    path('user/<int:id>/deposit/', deposit, name='deposit'),
-    path('user/<int:id>/withdraw/', withdraw, name='withdraw'),
 ]
