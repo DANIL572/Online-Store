afa24f01e22285d2569882b36208005a5e8dff42
Danil <danilsavrin93@gmail.com>
2025-12-16 17:25:25 +0300

    Добавил новое приложение в INSTALLED_APPS, а также переменные связанные с аутентификацией

--- So_Buy/settings.py
+++ So_Buy/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # Созданные приложения
     'User',
     'Registration',
+    'Auth'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'User.CustomUser'
+LOGIN_URL = '/auth/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
